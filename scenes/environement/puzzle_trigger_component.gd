f302080edcea56ffda078f4bbce2e59ae39ca43f
jlb-max <barras.jl@gmail.com>
2025-07-27 17:03:34 +0200

    added sunflower crop

--- scenes/environement/puzzle_trigger_component.gd
+++ scenes/environement/puzzle_trigger_component.gd
@@ -3,94 +3,81 @@ extends Area2D
 
 @export var required_plant: PlantData
 @export var template_scene_to_apply: PackedScene
-@export var target_game_tilemap: Node2D
+@export var target_game_tilemap: Node2D   # parent Node2D qui contient les TileMapLayer
 
 var listening_to_plant: PlantedCrop = null
 
-func _ready():
+func _ready() -> void:
     area_entered.connect(_on_area_entered)
     area_exited.connect(_on_area_exited)
 
-func _on_area_entered(area: Area2D):
+# --------------------------------------------------------------------------
+# 1. Détection d’entrée / sortie dans la zone
+# --------------------------------------------------------------------------
+func _on_area_entered(area: Area2D) -> void:
     if area.owner is PlantedCrop and area.owner.plant_data == required_plant:
         listening_to_plant = area.owner as PlantedCrop
         listening_to_plant.growth_cycle_component.crop_harvesting.connect(on_puzzle_solved)
 
-func _on_area_exited(area: Area2D):
+func _on_area_exited(area: Area2D) -> void:
     if area.owner == listening_to_plant:
-        if is_instance_valid(listening_to_plant) and listening_to_plant.growth_cycle_component.crop_harvesting.is_connected(on_puzzle_solved):
-            listening_to_plant.growth_cycle_component.crop_harvesting.disconnect(on_puzzle_solved)
+        if is_instance_valid(listening_to_plant):
+            var sig = listening_to_plant.growth_cycle_component.crop_harvesting
+            if sig.is_connected(on_puzzle_solved):
+                sig.disconnect(on_puzzle_solved)
         listening_to_plant = null
 
+# --------------------------------------------------------------------------
+# 2. Logique : apparition du pont
+# --------------------------------------------------------------------------
 func on_puzzle_solved() -> void:
-    # ----------------------------------------------------------------------
-    # 0. Vérifications rapides
-    # ----------------------------------------------------------------------
+    # 0. Vérifications
     if not template_scene_to_apply or not target_game_tilemap:
         return
 
-    # ----------------------------------------------------------------------
-    # 1. Récupère les couches nécessaires
-    # ----------------------------------------------------------------------
+    # 1. Couches nécessaires
     var template: TileMapLayer = template_scene_to_apply.instantiate()
-    var water_layer: TileMapLayer = target_game_tilemap.find_child("Water")
-    var grass_layer: TileMapLayer = target_game_tilemap.find_child("Grass")
-
+    var water_layer := target_game_tilemap.find_child("Water") as TileMapLayer
+    var grass_layer := target_game_tilemap.find_child("Grass") as TileMapLayer
     if not water_layer or not grass_layer:
-        push_error("Couches Water ou Grass introuvables.")
+        push_error("Couches « Water » ou « Grass » introuvables.")
         template.queue_free()
         return
 
-    # ------------------------------------------------------------------
-    # 2. Construit la liste des cases à traiter  (pont + voisins)
-    # ------------------------------------------------------------------
+    # 2. Cases du pont + voisines
     var cell_set: Dictionary = {}
     var template_cells: Array[Vector2i] = template.get_used_cells()
-
-    for cell: Vector2i in template_cells:
+    for cell in template_cells:
         cell_set[cell] = true
-        for n: Vector2i in grass_layer.get_surrounding_cells(cell):
+        for n in grass_layer.get_surrounding_cells(cell):
             cell_set[n] = true
-
-    # -- conversion sûre vers Array[Vector2i] --------------------------
     var bridge_cells: Array[Vector2i] = []
-    bridge_cells.assign(cell_set.keys())   # copy + typage OK
+    bridge_cells.assign(cell_set.keys())
 
-    # ----------------------------------------------------------------------
-    # 3. Regroupe les cases du pont par (terrain_set, terrain)
-    #    -> obligatoire car set_cells_terrain_connect() ne gère qu’un terrain à la fois
-    # ----------------------------------------------------------------------
+    # 3. Groupes par terrain
     var by_terrain: Dictionary = {}
-    for cell: Vector2i in template_cells:
+    for cell in template_cells:
         var td := template.get_cell_tile_data(cell)
-        if not td:
+        if td == null:
             continue
-        var key := Vector2i(td.terrain_set, td.terrain)   # (x = set, y = terrain)
+        var key := Vector2i(td.terrain_set, td.terrain)
         by_terrain[key] = by_terrain.get(key, []) + [cell]
 
-    # ----------------------------------------------------------------------
-    # 4. Efface d’abord toute case d’eau qui sera recouverte par le pont
-    # ----------------------------------------------------------------------
-    for cell: Vector2i in bridge_cells:
-        water_layer.erase_cell(cell)
+    ## 4. Efface l’eau
+    #for cell in bridge_cells:
+        #water_layer.erase_cell(cell)
 
-    # ----------------------------------------------------------------------
-    # 5. Peint les terrains et laisse Godot recoller bords & coins
-    # ----------------------------------------------------------------------
-    for terrain_key: Vector2i in by_terrain.keys():
+    # 5. Peint le pont (terrain connect)
+    for key: Vector2i in by_terrain.keys():
         grass_layer.set_cells_terrain_connect(
-            by_terrain[terrain_key],   # cellules à peindre (pont)
-            terrain_key.x,             # terrain_set
-            terrain_key.y              # terrain_id
+            by_terrain[key],
+            key.x,   # terrain_set
+            key.y    # terrain_id
         )
 
-    # ----------------------------------------------------------------------
-    # 6. Force le rafraîchissement immédiat (optionnel mais sûr)
-    # ----------------------------------------------------------------------
+    # 6. Rafraîchit immédiatement
     grass_layer.update_internals()
 
-    # ----------------------------------------------------------------------
     # 7. Nettoyage
-    # ----------------------------------------------------------------------
-    template.queue_free()   # on n’a plus besoin du modèle
-    queue_free()            # le trigger s’auto‑détruit (facultatif)
+    template.queue_free()
+    queue_free()            # supprime le trigger (facultatif)
